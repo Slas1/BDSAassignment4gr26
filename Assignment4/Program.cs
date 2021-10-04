49c0d15c42ba208fcf06864e2252f9f7661ce6e4
Tessa June Strand <tejs@itu.dk>
2021-10-04 18:12:16 +0200

    worked one some of the exercises.

--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Lecture04.Entities;
 
 namespace Assignment4
 {
@@ -6,7 +13,34 @@ namespace Assignment4
     {
         static void Main(string[] args)
         {
+            var configuration = LoadConfiguration();
+            var connectionString = configuration.GetConnectionString("Futurama");
+
             Console.WriteLine("Hello World!");
+            
+            var connection = new SqlConnection(connectionString);
+
+            // var cmdText = "SELECT * FROM Users";
+
+            var command = new SqlCommand(cmdText, connection);
+
+            connection.Open();
+
+            // open up command and get reader 
+            var reader = command.ExecuteReader();
+
+            connection.Close();
         }
+
+        static IConfiguration LoadConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddUserSecrets<Program>(); // related to Program. Maps to Assignment4.csproj.
+
+            return builder.Build();
+        }
+
     }
 }
