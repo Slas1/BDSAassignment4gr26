54391e9d1d2bdc50f071139ce29daf1ae4118164
Tessa June Strand <tejs@itu.dk>
2021-10-08 14:04:21 +0200

    modified code

--- Assignment4.Entities/KanbanContext.cs
+++ Assignment4.Entities/KanbanContext.cs
@@ -25,7 +25,6 @@ public class KanbanContext : DbContext
                     v => (State)Enum.Parse(typeof(State), v));
                 entity.HasMany(e => e.Tags).WithMany(e => e.Tasks);
                 });
-
         }
     }
 }
